fd6d8c96b7d7598a5172745450448c06f20d10b6
eleicha <laura.mons@web.de>
2024-12-18 17:00:28 +0100

    added to gitignore

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -1,9 +1,10 @@
-import org.example.Calculator;
+package org.example;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class MainTest {
+class CalculatorTest {
 
     @Test
     void testAddition() {
